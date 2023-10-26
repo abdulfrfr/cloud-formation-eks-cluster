c29aa54aaf1535eb1148ecdfae216f5d1df68ae2
ABDULSALAM ABDULRAZAQ <abdulmails112@gmail.com>
2023-10-26 02:22:18 +0100

    Update create-vpc-stack.sh

--- create-vpc-stack.sh
+++ create-vpc-stack.sh
@@ -1,4 +1,4 @@
 aws cloudformation create-stack \
   --region us-east-1 \
   --stack-name my-eks-vpc \
-  --template-body file://eks-vpc-stack.yaml 
+  --template-body file://hostober_challenge_cloudformation_networking.yaml 
